e49ad5e22a3c545ab06694e394e6ad4eb0b23d4d
Lucian Fialho <lucianfialhobp@hotmail.com>
2022-11-02 12:41:14 -0300

    -- copiando manifest e icons para pasta dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,41 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path")
 module.exports = {
     mode: "development",
     entry: "./src/test.js",
     module: {
         rules: [{
             test: /\.js$/,
-            use: 'raw-loader',
+            use: "raw-loader",
             exclude: /node_modules/
         }],
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { 
+                    from: path.resolve("src/manifest.json"),
+                    to: path.resolve("dist")
+                },
+                { 
+                    from: path.resolve("src/assets/icon-16x16.png"),
+                    to: path.resolve("dist")
+                },
+                { 
+                    from: path.resolve("src/assets/icon-38x38.png"),
+                    to: path.resolve("dist")
+                },
+                { 
+                    from: path.resolve("src/assets/icon-48x48.png"),
+                    to: path.resolve("dist")
+                },
+                { 
+                    from: path.resolve("src/assets/icon-128x128.png"),
+                    to: path.resolve("dist")
+                },
+            ]
+        })
+    ],
     output: {
         filename: "index.js"
     }
