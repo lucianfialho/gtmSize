299c15299f6342a0f197f989b9ea181f3b49de16
Lucian Fialho <lucianfialhobp@hotmail.com>
2022-11-07 16:23:52 -0300

    -- bugfix link sendo indicado como malicioso

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -8,7 +8,6 @@ function App () {
     useEffect(() => {
         
         chrome.runtime.sendMessage("teste", function(response) {
-                
             setContainerSize(
                 { 
                     result: response.result, 
@@ -57,7 +56,7 @@ function App () {
                                 </li>
                                 <li className="flex items-center">
                                     <svg className="w-4 h-4 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                                    {`Follow me on instagram `} &nbsp; <a className="font-medium" href="https://instagram.com.br/lucianfialho">@lucianfialho</a>
+                                    {`Follow us on instagram `} &nbsp; @metricasboss
                                 </li>
                             </ul>
                         </div>
